f9530213c936dbcea92c9342f9c00b526dedc38e
M.Zain.Riaz <233597@students.au.edu.pk>
2024-10-16 15:23:29 +0500

    Add files via upload

---
+++ Lab4/VP Lab 05/Activity 01/Activity 01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace act1
+{
+    class hello
+    {
+        static void Main(string[] args)
+        {
+            double a = 98, b = 0;
+
+            double result = 0;
+
+
+            try
+            {
+                result = SafeDivision(a, b);
+                Console.WriteLine($"{a} divided by {b} = {result}");
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Attempt Divided by Zero");
+            }
+        }
+
+        static double SafeDivision(double x, double y)
+        {
+            if (y != 0)
+            {
+                return x / y; // This line will be executed if y is not 0
+            }
+            else
+            {
+                throw new System.DivideByZeroException();
+            }
+        }
+    }
+
+
+}
+
+
