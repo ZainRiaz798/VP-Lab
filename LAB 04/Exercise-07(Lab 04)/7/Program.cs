0c299dc0d2825e8edb49a86de6e78815cc343545
M.Zain.Riaz <233597@students.au.edu.pk>
2024-10-23 14:19:14 +0500

    Add files via upload

---
+++ LAB 04/Exercise-07(Lab 04)/7/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Ex
+{
+    class MatrixMultiplication
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrixA = new int[3, 3];
+            int[,] matrixB = new int[3, 3];
+            int[,] resultMatrix = new int[3, 3];
+
+            Console.WriteLine("Enter elements of Matrix A (3x3):");
+            InputMatrix(matrixA);
+
+            Console.WriteLine("Enter elements of Matrix B (3x3):");
+            InputMatrix(matrixB);
+
+            resultMatrix = MultiplyMatrices(matrixA, matrixB);
+
+            Console.WriteLine("Resultant Matrix (A x B):");
+            DisplayMatrix(resultMatrix);
+        }
+
+        static void InputMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Element [{i + 1}, {j + 1}]: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+
+        static int[,] MultiplyMatrices(int[,] matrixA, int[,] matrixB)
+        {
+            int[,] result = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result[i, j] = 0; // Initialize result element
+                    for (int k = 0; k < 3; k++)
+                    {
+                        result[i, j] += matrixA[i, k] * matrixB[k, j];
+                    }
+                }
+            }
+            return result;
+        }
+
+        static void DisplayMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
