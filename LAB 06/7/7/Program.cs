07e6aa1ced81992866c53bc5b71fcfc02b1c4eb6
M.Zain.Riaz <233597@students.au.edu.pk>
2024-10-23 15:00:40 +0500

    Add files via upload

---
+++ LAB 06/7/7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExIList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int> intList = new List<int> { 10, 20, 30, 40 };
+            for (int i = 0; i < intList.Count; i++)
+            {
+                Console.WriteLine(intList[i]);
+            }
+        }
+    }
+}
+
