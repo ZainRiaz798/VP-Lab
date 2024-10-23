7a59ca51e4cdc94c5b1896beadf240a9e4ab47f9
M.Zain.Riaz <233597@students.au.edu.pk>
2024-10-23 14:58:08 +0500

    Add files via upload

---
+++ LAB 06/1/Activity 01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExIList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int> intList = new List<int>();
+            intList.Add(10);
+            intList.Add(20);
+            intList.Add(30);
+            intList.Add(40);
+        }
+    }
+}
---
+++ LAB 06/2/2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExIList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> strList = new List<string>();
+            strList.Add("One");
+            strList.Add("Two");
+            strList.Add("Three");
+            strList.Add("Four");
+            strList.Add("Four");  // Duplicate value
+            strList.Add("null");  // Adding the string "null"
+            strList.Add("null");  // Duplicate string "null"
+        }
+    }
+}
+
