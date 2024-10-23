724c53b888b70375d95b8fe30eedb1bdb644ca2e
M.Zain.Riaz <233597@students.au.edu.pk>
2024-10-23 15:00:09 +0500

    Add files via upload

---
+++ LAB 06/3/3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExIList
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> studentList = new List<Student>();
+            studentList.Add(new Student());
+            studentList.Add(new Student());
+            studentList.Add(new Student());
+        }
+    }
+}
+
---
+++ LAB 06/4/4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExIList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int> intList = new List<int> { 10, 20, 30, 40 };
+        }
+    }
+}
+
