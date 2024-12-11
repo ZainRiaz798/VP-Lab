1e6222f109ffde96446082c7b0f48111db2426ff
M.Zain.Riaz <233597@students.au.edu.pk>
2024-12-11 15:25:07 +0500

    Add files via upload

---
+++ Lab 11/1/Task-01/Task-01/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace _233599_Task_01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class BringBackPocoyo : Window
+    {
+        public BringBackPocoyo()
+        {
+            InitializeComponent();
+        }
+    }
+}
