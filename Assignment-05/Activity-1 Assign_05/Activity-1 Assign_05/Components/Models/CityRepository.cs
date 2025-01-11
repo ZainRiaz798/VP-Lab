540496d376533f7df08b4ac78bb65d2076a26779
M.Zain.Riaz <233597@students.au.edu.pk>
2025-01-11 20:07:45 +0500

    Add files via upload

---
+++ Assignment-05/Activity-1 Assign_05/Activity-1 Assign_05/Components/Models/CityRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Activity_1_Assign_05.Components.Models
+{
+    public class CityRepository
+    {
+        public static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+        public static List<string> GetCities() => cities;
+    }
+}
+
+
